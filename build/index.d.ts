4f74aa15af13581ccd6f04a852a4ea5c78250a65
TarVK <tarvankrieken@gmail.com>
2021-03-30 15:26:31 +0200

    Initialized some project structure

---
+++ build/index.d.ts
@@ -0,0 +1,2 @@
+export declare const doStuff: (message: string) => void;
+//# sourceMappingURL=index.d.ts.map
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export const doStuff = (message: string)=>{
+    console.log(message);
+}

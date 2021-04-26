34e9c8ee17ee29037731f038480a39cbef0da6ae
Sancarn <sancarn@hotmail.co.uk>
2021-04-26 19:57:21 +0100

    temporary test change

--- src/test.ts
+++ src/test.ts
@@ -1,34 +1,34 @@
 import {GlobalKeyboardListener} from ".";
 const v = new GlobalKeyboardListener();
 v.addListener(function(e){
-  console.log(e);
+  console.log(e.name + " " + (e.state =="DOWN" ? "DOWN" : "UP  ")+ "       [" + e.rawKey._nameRaw + "]");
 });
-v.addListener(function(e){
-  if (e.name == "A"){
-    console.log("A Halt remote propagation");
-    return true;
-  }
-});
-v.addListener(function(e){
-  if(e.name == "S"){
-    console.log("S Halt remote propagation");
-    return {stopPropagation: true};
-  }
-});
-v.addListener(function(e){
-  if(e.name == "D"){
-    console.log("D Halt listener propagation only");
-    return {stopImmediatePropagation: true};
-  }
-});
-v.addListener(function(e){
-  if(e.name == "D"){
-    console.log("This shouldn't call D");
-    return {stopImmediatePropagation: e.name == "D"};
-  }
-});
-v.addListener(function(e){
-  if(e.name == "B"){
-    v.kill();
-  }
-});
+// v.addListener(function(e){
+//   if (e.name == "A"){
+//     console.log("A Halt remote propagation");
+//     return true;
+//   }
+// });
+// v.addListener(function(e){
+//   if(e.name == "S"){
+//     console.log("S Halt remote propagation");
+//     return {stopPropagation: true};
+//   }
+// });
+// v.addListener(function(e){
+//   if(e.name == "D"){
+//     console.log("D Halt listener propagation only");
+//     return {stopImmediatePropagation: true};
+//   }
+// });
+// v.addListener(function(e){
+//   if(e.name == "D"){
+//     console.log("This shouldn't call D");
+//     return {stopImmediatePropagation: e.name == "D"};
+//   }
+// });
+// v.addListener(function(e){
+//   if(e.name == "B"){
+//     v.kill();
+//   }
+// });

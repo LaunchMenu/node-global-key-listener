fb77fe0b4492a47445e7df52c673b54b93f24f7c
Sancarn <sancarn@hotmail.co.uk>
2021-04-26 01:07:31 +0100

    minor formatting

--- src/test.ts
+++ src/test.ts
@@ -1,36 +1,34 @@
-import {GlobalKeyboardListener} from "."
+import {GlobalKeyboardListener} from ".";
 const v = new GlobalKeyboardListener();
 v.addListener(function(e){
   console.log(e.key.name);
 });
 v.addListener(function(e){
   if (e.key.name == "A"){
-      console.log("Halt A")
-      return true;
+    console.log("A Halt remote propagation");
+    return true;
   }
 });
 v.addListener(function(e){
   if(e.key.name == "S"){
-      console.log("Halt S")
-      return {stopPropagation: true};
+    console.log("S Halt remote propagation");
+    return {stopPropagation: true};
   }
 });
 v.addListener(function(e){
   if(e.key.name == "D"){
-      console.log("Halt D Immediate")
-      return {stopImmediatePropagation: true};
+    console.log("D Halt listener propagation only");
+    return {stopImmediatePropagation: true};
   }
-  
 });
 v.addListener(function(e){
   if(e.key.name == "D"){
-      console.log("This shouldn't call D")
-      return {stopImmediatePropagation: e.key.name == "D"};
+    console.log("This shouldn't call D");
+    return {stopImmediatePropagation: e.key.name == "D"};
   }
 });
-
 v.addListener(function(e){
   if(e.key.name == "B"){
-      v.stop()
+    v.stop();
   }
 });

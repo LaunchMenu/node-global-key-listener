39ead05e4c4b68a21dbb45fdafa30f86fd67f8e8
Tar <tarvankrieken@gmail.com>
2021-05-31 21:43:24 +0200

    Fix/install (#10)
    * Removed run-script-os
    
    * Removed forgotten scripts
    
    * Properly allowed for sudo

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,8 +1,10 @@
-const {exec} = require("child_process");
-
 if (process.platform == "darwin") {
     console.log("Post install:");
-    exec("sudo chmod +x bin/MacKeyServer", (error, stdout, stderr) => {
+    const sudo = require("sudo-prompt");
+    const options = {
+        name: "Global key listener",
+    };
+    sudo.exec("chmod +x bin/MacKeyServer", options, (error, stdout, stderr) => {
         if (error) {
             console.error(`exec error: ${error}`);
             return;
